f7a21df1fa2542de0052b833474d9a1c416a4bee
Jørgen Årnes <36748061+jorgenaa@users.noreply.github.com>
2022-06-28 15:32:14 +0200

    removed unused section tag

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,42 +1,37 @@
-import { ToastContainer, toast } from "react-toastify"
-import 'react-toastify/dist/ReactToastify.css';
-import { SubmitHandlerFunc } from "../../framework/common/types/form"
-import Form from "@components/Form/Form";
-import { Heading } from "@components/ui";
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { SubmitHandlerFunc } from '../../framework/common/types/form'
+import Form from '@components/Form/Form'
+import { Heading } from '@components/ui'
 import * as ContactSection from '../../components/ContactSections'
-import { Layout } from "../../components/common";
-import s from "./contact.module.css"
+import { Layout } from '../../components/common'
+import s from './contact.module.css'
 
 const Contact = () => {
-   
-    const handleSubmit: SubmitHandlerFunc = async (state, callback) => { 
-     
-        const inputValue = JSON.stringify({
-            email: state.email,
-            name: state.name,
-            subject: state.subject,
-            message: state.message,
-        })
-    
-        callback() 
-    }
+  const handleSubmit: SubmitHandlerFunc = async (state, callback) => {
+    const inputValue = JSON.stringify({
+      email: state.email,
+      name: state.name,
+      subject: state.subject,
+      message: state.message,
+    })
 
-    return (
-        <div className={s.contact}>
-            <Heading title="Contact Us" classType={s.contact__heading} />
-            <ToastContainer />
-            <section className={s.contact__container}>
-                <ContactSection.ContactInfo />
-                <Form handleSubmit={handleSubmit} />
-            </section>
-            <section className={s.contact__map}>
-                <ContactSection.ContactMap />
-            </section>
-           
-        </div>
-    )
+    callback()
+  }
+
+  return (
+    <div className={s.contact}>
+      <Heading title='Contact Us' classType={s.contact__heading} />
+      <ToastContainer />
+      <section className={s.contact__container}>
+        <ContactSection.ContactInfo />
+        <Form handleSubmit={handleSubmit} />
+      </section>
+      <ContactSection.ContactMap />
+    </div>
+  )
 }
 
-Contact.Layout = Layout;
+Contact.Layout = Layout
 
-export default Contact;
+export default Contact
